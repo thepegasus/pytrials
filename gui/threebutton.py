baa0dffb057a6791c60257e5f0fcae1911b432df
thepegasus <thepegasus@users.noreply.github.com>
2014-05-03 20:46:03 +0530

    Responding to events from keyboard

--- gui/threebutton.py
+++ gui/threebutton.py
@@ -17,6 +17,7 @@ class MyApp:
         self.button2.configure(background="tan")
         self.button2.pack(side=LEFT)
         self.button2.bind("<Button-1>", self.button2Click)
+        self.button2.bind("<Return>", self.button2Click)
         
         self.button3=Button(self.myContainer1)
         self.button3.configure(text="Join me?", background="cyan")
@@ -27,12 +28,22 @@ class MyApp:
 
     def button1Click(self, event):
         if self.button1["background"]=="green":
-           self.button1["background"]="red"
+            self.button1["background"]="red"
         else:
-                self.button1["background"]="blue"
+            self.button1["background"]="blue"
     def button2Click(self, event):
-        self.myParent.destroy()
+        report_event(event)
+#        self.myParent.destroy()
 
+def report_event(event):
+    """Print a description of an event based on its attributes.
+    """
+    event_name={"2":"Keypress", "4":"ButtonPress"}
+    print "Time:", str(event.time)
+    print "EventType:" + str(event.type), \
+        event_name[str(event.type)], \
+        "EventWidgetID="+ str(event.widget), \
+        "EventKeySymbol=" + str(event.keysym)
 
 root=Tk()
 #root.title="Test"
