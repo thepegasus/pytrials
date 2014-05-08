b3adcfdbce18680da08f8a9c0a580e70940a4b30
thepegasus <thepegasus@users.noreply.github.com>
2014-05-08 23:44:09 +0530

    Big Diff

---
+++ minmax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#Given an array length 1 or more of ints, return the difference between the largest and smallest values in the array. Note: the built-in min(v1, v2) and max(v1, v2) functions return the smaller or larger of two values.
+
+#big_diff([10, 3, 5, 6]) - 7
+#big_diff([7, 2, 10, 9]) - 8
+#big_diff([2, 10, 7, 2]) - 8
+
+def big_diff(nums):
+    diff=max(nums)-min(nums)
+    print(diff)
+
+
+big_diff([10, 3, 5, 6])
+big_diff([7, 2, 10, 9]) 
+big_diff([2, 10, 7, 2])
